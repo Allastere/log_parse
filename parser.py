1749ff10181db0d8514726e00d342e4e02e9f764
Egor <egorenka2019@gmail.com>
2026-09-03 21:12:48 +0700

    Первый коммит: добавлен проект

---
+++ parser.py
@@ -0,0 +1,32 @@
+def log_parse(filename):
+    logs = []
+    with open(filename, "r") as file:
+        for line in file:
+            parts = line.split()
+            if "Failed password" in line:
+                log = {
+                    "date" : parts[0] + " " + parts[1],
+                    "time" : parts[2],
+                    "event" : parts[5] + " " + parts[6],
+                    "user" : parts[8],
+                    "ip" : parts[10],
+                    "port" : parts[12]
+                }
+                logs.append(log)
+    with open(filename, "r") as file:
+        for line in file:
+            parts = line.split()
+            if "Accepted password" in line:
+                log = {
+                    "date" : parts[0] + " " + parts[1],
+                    "time" : parts[2],
+                    "event" : parts[5] + " " + parts[6],
+                    "user" : parts[8],
+                    "ip" : parts[10],
+                    "port" : parts[12]
+                }
+                logs.append(log)
+    return logs
+
+logs = log_parse("ssh.log")
+print(logs)
