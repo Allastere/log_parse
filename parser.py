665c1e869d55885e55f7c65b839add2682995619
Egor <egorenka2019@gmail.com>
2026-09-05 18:50:01 +0700

    Fixed the bug

--- parser.py
+++ parser.py
@@ -13,7 +13,6 @@ def log_parse(filename):
                     "port" : parts[12]
                 }
                 logs.append(log)
-    with open(filename, "r") as file:
         for line in file:
             parts = line.split()
             if "Accepted password" in line:
@@ -28,5 +27,25 @@ def log_parse(filename):
                 logs.append(log)
     return logs
 
+
+def detect_suspicious_ips(logs, threshold = 3):
+    failed_attempts = {}
+    for log in logs:
+        if log["event"] == "Failed password":
+            ip = log["ip"]
+            failed_attempts[ip] = failed_attempts.get(ip, 0) + 1
+    suspicious_ips = {}
+
+    for ip, attempts in failed_attempts.items():
+        if attempts >= threshold:
+            suspicious_ips[ip] = attempts
+
+    return suspicious_ips
+
+def print_suspicious_ips(suspicious_ips):
+    for ip, attempts in suspicious_ips.items():
+        print(f"IP: {ip} | Failed attempts: {attempts}")
+
 logs = log_parse("ssh.log")
-print(logs)
+failed  = detect_suspicious_ips(logs, 1)
+print_suspicious_ips(failed)
